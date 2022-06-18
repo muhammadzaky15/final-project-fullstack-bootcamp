266cd97c6944cf24f789b3580b56c26bc8eaad68
muhammadzakyhol <muhammad.zaky.pnl@gmail.com>
2022-06-18 14:10:17 +0700

    add config for heroku

--- server/config/db.js
+++ server/config/db.js
@@ -1,7 +1,11 @@
 const config = {
-    host: "localhost",
-    user: "root",
-    database: "lemangshop"
+    host: "477upt3rgq3u.ap-southeast-2.psdb.cloud",
+    user: "dzn2pjbcjt6i",
+    password: "pscale_pw_194iTjtZauFLybVL5ffut21pZXVV72L8ic9zQl_-MuA",
+    database: "finalproject",
+    ssl: {
+        rejectUnauthorized: false
+    }
 }
 
 module.exports = config;
--- server/routes/products.js
+++ server/routes/products.js
@@ -31,4 +31,10 @@ router.post("/", middlewareVerifyToken, productValidator(), async function(req, 
     res.status(createProduct.status).send(createProduct);
 })
 
+router.get("/:id", async function(req, res) {
+    res.send({
+        name: "testing aja"
+    })
+})
+
 module.exports = router;
